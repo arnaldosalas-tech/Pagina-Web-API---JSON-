01ae95dfd0361e716195c54b3d8177ac9a3f3e55
arnaldosalas-tech <arnaldosalas777@gmail.com>
2025-11-02 13:38:22 -0400

    Update URL in my-page.js

--- my-page.js
+++ my-page.js
@@ -1,5 +1,5 @@
 ﻿data = {
     name: "ARNALDO SALAS",
     matricula: "20231498",
-    url: "https://TU-SITIO-NETLIFY.netlify.app/" // <-- actualiza con tu URL real y deja la barra final
+    url: "https://webpageapijson.netlify.app/" // <-- actualiza con tu URL real y deja la barra final
 }
